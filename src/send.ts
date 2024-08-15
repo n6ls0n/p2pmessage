6c5c629b1a902d537f58777267aef061bb3daade
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-08-15 10:41:53 -0400

    Added export and utility functions

--- src/send.ts
+++ src/send.ts
@@ -1,4 +1,6 @@
 import { Peer, DataConnection } from "peerjs";
+// Note: All args of type HTMLElement are global variables defined within index.ts. The observant reader should infer this based on the fact that there are no return values.
+
 
 // Create the Peer object for our end of the connection.
 // Sets up callbacks that handle any events related to our peer object.
@@ -38,100 +40,97 @@ export function initialize(peer: Peer, status: HTMLElement) {
 
 // //  Create the connection between the two Peers.
 // //  Sets up callbacks that handle any events related to the connection and data received on it.
-// export function join(peer: Peer, recvIdInput: HTMLInputElement, status: HTMLElement, message: HTMLElement) {
-//     // TODO: Add code to handle case where a connection to a peer already exists and a new connection needs to be made then remove the code below
-//     // Close old connection
-//     // if (conn) {
-//     //     conn.close();
-//     // }
+export function join(peer: Peer, recvIdInput: HTMLInputElement, status: HTMLElement, message: HTMLElement) {
+    // TODO: Add code to handle case where a connection to a peer already exists and a new connection needs to be made then remove the code below
+    // Close old connection
+    // if (conn) {
+    //     conn.close();
+    // }
 
-//     // Create connection to destination peer specified in the input field
-//     var conn: DataConnection = peer.connect(recvIdInput.value, {
-//         reliable: true
-//     });
+    // Create connection to destination peer specified in the input field
+    var conn: DataConnection = peer.connect(recvIdInput.value, {
+        reliable: true
+    });
 
-//     // Return status message
-//     conn.on('open', function () {
-//         status.innerHTML = "Connected to: " + conn.peer;
-//         console.log("Connected to: " + conn.peer);
-//         // Check URL params for commands that should be sent immediately
-//         var command = getUrlParam("command");
-//         if (command)
-//             conn.send(command);
-//     });
+    // Return status message
+    conn.on('open', function () {
+        status.innerHTML = "Connected to: " + conn.peer;
+        console.log("Connected to: " + conn.peer);
+        // Check URL params for commands that should be sent immediately
+        var command = getUrlParam("command");
+        if (command)
+            conn.send(command);
+    });
 
-//     // Handle incoming data (messages only since this is the signal sender)
-//     conn.on('data', function (data) {
-//         addMessage("<span class=\"peerMsg\">Peer:</span> " + data, message);
-//     });
+    // Handle incoming data (messages only since this is the signal sender)
+    conn.on('data', function (data) {
+        addMessage("<span class=\"peerMsg\">Peer:</span> " + data, message);
+    });
 
-//     conn.on('close', function () {
-//         if (status !== null){
-//         status.innerHTML = "Connection closed";
-//         }
-//     });
-// };
+    conn.on('close', function () {
+        if (status !== null){
+        status.innerHTML = "Connection closed";
+        }
+    });
+};
 
-// //  Get first "GET style" parameter from href.
-// //  This enables delivering an initial command upon page load.
-// //  TODO: Would have been easier to use location.hash.(Original Author comment)
-// export function getUrlParam(name: string) {
-//     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-//     var regexS = "[\\?&]" + name + "=([^&#]*)";
-//     var regex = new RegExp(regexS);
-//     var results = regex.exec(window.location.href);
-//     if (results == null)
-//         return null;
-//     else
-//         return results[1];
-// };
+//  Send a signal via the peer connection and add it to the log.
+//  This will only occur if the connection is still alive.
+export function signal(sigName: string, message: HTMLElement, conn: DataConnection) {
+    var cueString = "<span class=\"cueMsg\">Cue: </span>";
+    if (conn && conn.open) {
+        conn.send(sigName);
+        console.log(sigName + " signal sent");
+        addMessage(cueString + sigName, message);
+    } else {
+        console.log('Connection is closed');
+    }
+};
 
-// /**
-//  * Adds a message to the specified HTML element with the current time.
-//  *
-//  * @param {string} msg - The message to be added.
-//  * @param {HTMLElement} message - The HTML element where the message will be added.
-//  * @return {HTMLElement} The updated HTML element with the added message.
-//  */
-// function addZero(t: number) : string {
-//     if (t < 10) {
-//         return "0" + String(t);
-//     } else {
-//         return String(t);
-//     }
-// }
+// Adds a message to the specified HTML element with the current time.
+export function addMessage(msg: string, message: HTMLElement) {
+    var now = new Date();
+    var h = now.getHours();
+    var m = addZero(now.getMinutes());
+    var s = addZero(now.getSeconds());
+    if (h > 12)
+        h -= 12;
+    else if (h === 0)
+        h = 12;
+    message.innerHTML = "<br><span class=\"msg-time\">" + h + ":" + m + ":" + s + "</span>  -  " + msg + message.innerHTML;
+    return message;
+};
 
-// export function addMessage(msg: string, message: HTMLElement) {
-//     var now = new Date();
-//     var h = now.getHours();
-//     var m = addZero(now.getMinutes());
-//     var s = addZero(now.getSeconds());
-//     if (h > 12)
-//         h -= 12;
-//     else if (h === 0)
-//         h = 12;
-//     message.innerHTML = "<br><span class=\"msg-time\">" + h + ":" + m + ":" + s + "</span>  -  " + msg + message.innerHTML;
-//     return message;
-// };
+// Clears the messages in the specified HTML element via adding an empty string and then another string.
+export function clearMessages(message: HTMLElement) {
+    message.innerHTML = "";
+    addMessage("Msgs cleared",message);
+};
 
-//  export function clearMessages(message: HTMLElement) {
-//     message.innerHTML = "";
-//     addMessage("Msgs cleared",message);
-// };
+// Utility function used in the addMessage function
+function addZero(t: number) : string {
+    if (t < 10) {
+        return "0" + String(t);
+    } else {
+        return String(t);
+    }
+};
 
-// //  Send a signal via the peer connection and add it to the log.
-// //  This will only occur if the connection is still alive.
-// //  "message" and "conn" are global variables defined in
-// export function signal(sigName: string, message: HTMLElement, conn: DataConnection) {
-//     var cueString = "<span class=\"cueMsg\">Cue: </span>";
-//     if (conn && conn.open) {
-//         conn.send(sigName);
-//         console.log(sigName + " signal sent");
-//         addMessage(cueString + sigName, message);
-//     } else {
-//         console.log('Connection is closed');
-//     }
-// };
+//  Utility function used in the join function
+//  Get first "GET style" parameter from href.
+//  This enables delivering an initial command upon page load.
+//  TODO: Would have been easier to use location.hash.(Original Author comment)
+function getUrlParam(name: string) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return null;
+    else
+        return results[1];
+};
+
 
 
 
